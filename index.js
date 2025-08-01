a839ef80520498f9dccb61034cbac39b25600d1f
Magnus Andersen <magnuan@gmail.com>
2025-08-01 08:28:00 +0200

    debug

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = (app) => {
         //app.debug(`Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}`);
         // Skip short packets
         if (msg.length < 6) {
-            app.debug(`Too short`);
+            //app.debug(`Too short`);
             return;
         }
           
@@ -36,21 +36,21 @@ module.exports = (app) => {
         // IP filtering if a specific IP is set
         const sourceIP = rinfo.address;
         if (ip !== '0.0.0.0' && sourceIP !== ip) {
-            app.debug(`Wrong IP`);
+            //app.debug(`Wrong IP`);
             return;
         }
 
         //Check that message starts as a PosMV GRP message
         const prefix = msg.toString('ascii', 0, 4);
         if (prefix !== '$GRP') {
-            app.debug(`Wrong Start`);
+            //app.debug(`Wrong Start`);
             // Skip unrelated messages
             return;
         }
         //Only process GID=1
         const gid = msg.readUInt16LE(4);
         if (gid !== 1) {
-            app.debug(`Wrong gid`);
+            //app.debug(`Wrong gid`);
             return;
         }
 
@@ -61,7 +61,7 @@ module.exports = (app) => {
         const minInterval = 1000 / rate;
 
         if (lastMessageTime && (now - lastMessageTime) < minInterval) {
-          app.debug(`Rate skip`);
+          //app.debug(`Rate skip`);
           return;
         }
         lastMessageTime = now;
